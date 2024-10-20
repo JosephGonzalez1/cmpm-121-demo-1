ce127d158155e149bc3bce5f1645f63144ced785
JosephGonzalez1 <152119091+JosephGonzalez1@users.noreply.github.com>
2024-10-19 18:21:24 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,9 @@ document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+const button = document.createElement("button");
+const img = document.createElement("img");
+img.src = 'src/assets/fire_1f525.png';
 app.append(header);
+app.append(button);
+button.append(img);
